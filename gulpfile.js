3e164a1dc46d59f9b63fba1875a64752d086824e
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-02-26 09:12:16 -0800

    Added gulp for copying css files. TS build not producing .d.ts files

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const tsProject = ts.createProject('tsconfig.json');
+const merge = require('merge2');
+
+// gulp.task('default', function () {
+//     const tsResult = tsProject.src().pipe(tsProject());
+//     return merge([
+//         tsResult.js.pipe(gulp.dest('dist')),
+//         tsResult.dts.pipe(gulp.dest('dist')),
+//     ]);
+// });
+
+gulp.task('copyCSS', function () {
+    return gulp.src(['src/**/*.css', 'src/**/**/*.css'])
+        .pipe(gulp.dest('dist'));
+});
